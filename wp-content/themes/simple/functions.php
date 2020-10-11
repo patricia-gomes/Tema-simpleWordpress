de5cfb0e42687c348f1e70d7bfc5ede34645de61
Patricia Gomes <patriciagomesro33@gmail.com>
2020-10-10 22:36:00 -0400

    Add Widgets

--- wp-content/themes/simple/functions.php
+++ wp-content/themes/simple/functions.php
@@ -3,3 +3,4 @@ require get_template_directory().'/include/helper.php';
 
 add_action('wp_enqueue_scripts', 'ps_theme_styles');
 add_action('after_setup_theme', 'ps_after_setup_theme');
+add_action('wdigets_init', 'ps_widgets');
