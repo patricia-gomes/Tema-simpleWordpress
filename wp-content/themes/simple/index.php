75a62a10c8934d80e2df5746e3c611510871afa3
Patricia Gomes <patriciagomesro33@gmail.com>
2020-10-12 22:51:47 -0400

    Add o html principal onde contém o titulo, data, imagem e conteúdo do artigo na página inicial do blog

--- wp-content/themes/simple/index.php
+++ wp-content/themes/simple/index.php
@@ -2,12 +2,37 @@
 	
 <section>
 	<div class="container">
-		Container
+		<div class="article">
+			<!--Verificando se existe post cadastrado-->
+			<?php if(have_posts()): ?>
+				<?php while(have_posts()): ?>
+					<?php the_post(); ?>
+					<!--Exibe o titulo-->
+					<h2 ><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					<!--Exibe a data-->
+					<div class="article_date"><?php echo get_the_date(); ?></div>
+					<!--Verificando se existe uma imagem nesse post-->
+					<?php if(has_post_thumbnail()): ?>
+						<!--Link do post na tag <a>-->
+						<a href="<?php the_permalink(); ?>">
+							<!--Exibe a imagem em tamanho original (full)-->
+							<?php the_post_thumbnail('full'); ?>
+						</a>
+					<?php endif; ?>
+					<!--Exibe o texto do artigo-->
+					<div class="article_text">
+						<p><?php the_excerpt(); ?></p>
+						<a href="<?php the_permalink(); ?>">Leia mais...</a>
+					</div>
+					<!--Exibe a quantidade de comentarios-->
+					<p class="comments_number">
+						<?php comments_number('0 comentários', '1 comentário', '% comentários'); ?>
+					</p>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div><!--Fim article-->
+		<?php get_sidebar(); ?>
 	</div>
-
-	<sidebar>
-		Side bar
-	</sidebar>
 </section>
 
 <?php get_footer(); ?>
