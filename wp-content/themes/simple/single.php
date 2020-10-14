cc9df3be1d678c8ef8f6b12c786cd098ce72f436
Patricia Gomes <patriciagomesro33@gmail.com>
2020-10-13 21:09:50 -0400

    Add a página do post especifico é parecido com a da página home com algumas pequenas alterações

---
+++ wp-content/themes/simple/single.php
@@ -0,0 +1,42 @@
+
+<?php get_header(); ?>
+	
+<section>
+	<div class="container">
+		<div class="article">
+			<!--Verificando se existe post cadastrado-->
+			<?php if(have_posts()): ?>
+				<?php while(have_posts()): ?>
+					<?php the_post(); ?>
+					<!--Exibe o titulo-->
+					<h2 class="widget_title"><?php the_title(); ?></h2>
+					<!--Exibe a data-->
+					<div class="article_date"><?php echo get_the_date(); ?></div>
+					<!--Verificando se existe uma imagem nesse post-->
+					<?php if(has_post_thumbnail()): ?>
+							<!--Exibe a imagem em tamanho original (full)-->
+							<?php the_post_thumbnail('full'); ?>
+					<?php endif; ?>
+					<!--Exibe o texto do artigo-->
+					<div class="article_text">
+						<p><?php the_content(); ?></p>
+					</div>
+					<!--Exibe a quantidade de comentarios-->
+					<p class="comments_number">
+						<?php comments_number('0 comentários', '1 comentário', '% comentários'); ?>
+					</p>
+				<?php endwhile; ?>
+			<?php endif; ?>
+			<!---Post anterior e o proximo post-->
+			<div class="pagination">
+				<div class="container_pagination">
+					<div class="previous_page"><?php previous_post_link(); ?></div>
+					<div class="next_page"><?php next_post_link(); ?></div>
+				</div>
+			</div>
+		</div><!--Fim article-->
+		<?php get_sidebar(); ?>
+	</div>
+</section>
+
+<?php get_footer(); ?>
