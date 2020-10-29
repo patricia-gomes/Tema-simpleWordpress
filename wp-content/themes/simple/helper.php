c7ab9e45704623fa082c3d276a848dd9cb2805b2
Patricia Gomes <patriciagomesro33@gmail.com>
2020-10-29 18:27:30 -0400

    Update Wordpress

--- wp-content/themes/simple/helper.php
+++
@@ -1,6 +0,0 @@
-<?php
-require get_template_directory().'/include/functions.php';
-
-add_action('wp_enqueue_scripts', 'ps_theme_styles');
-add_action('after_setup_theme', 'ps_after_setup_theme');
-add_action('widgets_init', 'ps_widgets');
